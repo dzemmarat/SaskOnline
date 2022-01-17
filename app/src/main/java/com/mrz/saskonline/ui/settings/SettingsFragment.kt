839aacb29048736243e7a2720a1ed6c4967e1da3
Марат Джеманкулов <m.dzhemankulov@profsoft.pro>
2022-01-17 17:06:07 +0300

    Create settings fragment

---
+++ app/src/main/java/com/mrz/saskonline/ui/settings/SettingsFragment.kt
@@ -0,0 +1,26 @@
+package com.mrz.saskonline.ui.settings
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.mrz.saskonline.databinding.FragmentMessagesBinding
+import com.mrz.saskonline.databinding.FragmentSettingsBinding
+import com.mrz.saskonline.ui.core.BaseFragment
+import com.mrz.saskonline.viewmodel.messages.MessagesViewModel
+import com.mrz.saskonline.viewmodel.settings.SettingsViewModel
+
+class SettingsFragment:
+    BaseFragment<SettingsViewModel, FragmentSettingsBinding>() {
+
+    override val viewModel: SettingsViewModel by viewModels()
+
+    override fun inflateViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentSettingsBinding =
+        FragmentSettingsBinding.inflate(inflater, container, false)
+
+    override fun setupViews() {
+
+    }
+}
---
+++ app/src/main/java/com/mrz/saskonline/viewmodel/settings/SettingsViewModel.kt
@@ -0,0 +1,6 @@
+package com.mrz.saskonline.viewmodel.settings
+
+import com.mrz.saskonline.viewmodel.core.BaseViewModel
+
+class SettingsViewModel: BaseViewModel() {
+}
