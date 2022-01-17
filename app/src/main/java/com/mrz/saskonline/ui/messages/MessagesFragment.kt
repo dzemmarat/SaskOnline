bcc176bcafb97a7ec071b18319ed4c4eb9f1f09d
Марат Джеманкулов <m.dzhemankulov@profsoft.pro>
2022-01-17 17:05:25 +0300

    Create messages fragment

---
+++ app/src/main/java/com/mrz/saskonline/ui/messages/MessagesFragment.kt
@@ -0,0 +1,24 @@
+package com.mrz.saskonline.ui.messages
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.mrz.saskonline.databinding.FragmentMessagesBinding
+import com.mrz.saskonline.ui.core.BaseFragment
+import com.mrz.saskonline.viewmodel.messages.MessagesViewModel
+
+class MessagesFragment:
+    BaseFragment<MessagesViewModel, FragmentMessagesBinding>() {
+
+    override val viewModel: MessagesViewModel by viewModels()
+
+    override fun inflateViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentMessagesBinding =
+        FragmentMessagesBinding.inflate(inflater, container, false)
+
+    override fun setupViews() {
+
+    }
+}
---
+++ app/src/main/java/com/mrz/saskonline/viewmodel/messages/MessagesViewModel.kt
@@ -0,0 +1,6 @@
+package com.mrz.saskonline.viewmodel.messages
+
+import com.mrz.saskonline.viewmodel.core.BaseViewModel
+
+class MessagesViewModel: BaseViewModel() {
+}
