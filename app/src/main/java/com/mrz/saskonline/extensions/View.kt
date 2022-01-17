6fe5614fe3f58a96042400e7200d2ae1cc82ccef
Марат Джеманкулов <m.dzhemankulov@profsoft.pro>
2022-01-17 17:07:30 +0300

    Add extensions

---
+++ app/src/main/java/com/mrz/saskonline/extensions/View.kt
@@ -0,0 +1,11 @@
+package com.mrz.saskonline.extensions
+
+import android.view.View
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
