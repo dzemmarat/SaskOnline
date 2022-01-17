5f1618a5c1815e397c50475a80b3c5e6b6d70604
Марат Джеманкулов <m.dzhemankulov@profsoft.pro>
2022-01-17 16:36:47 +0300

    Create notes fragment

---
+++ app/src/main/java/com/mrz/saskonline/ui/notes/NotesFragment.kt
@@ -0,0 +1,24 @@
+package com.mrz.saskonline.ui.notes
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.mrz.saskonline.databinding.FragmentNotesBinding
+import com.mrz.saskonline.ui.core.BaseFragment
+import com.mrz.saskonline.viewmodel.notes.NotesViewModel
+
+class NotesFragment:
+    BaseFragment<NotesViewModel, FragmentNotesBinding>() {
+
+    override val viewModel: NotesViewModel by viewModels()
+
+    override fun inflateViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentNotesBinding =
+        FragmentNotesBinding.inflate(inflater, container, false)
+
+    override fun setupViews() {
+
+    }
+}
---
+++ app/src/main/java/com/mrz/saskonline/viewmodel/notes/NotesViewModel.kt
@@ -0,0 +1,6 @@
+package com.mrz.saskonline.viewmodel.notes
+
+import com.mrz.saskonline.viewmodel.core.BaseViewModel
+
+class NotesViewModel: BaseViewModel() {
+}
