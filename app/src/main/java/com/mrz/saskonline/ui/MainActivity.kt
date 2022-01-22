9a69c223a9062d94deba8fb7a6f739cdec2d0415
Марат Джеманкулов <m.dzhemankulov@profsoft.pro>
2022-01-22 10:30:58 +0300

    Add navigation

--- app/src/main/java/com/mrz/saskonline/ui/MainActivity.kt
+++ app/src/main/java/com/mrz/saskonline/ui/MainActivity.kt
@@ -2,16 +2,30 @@ package com.mrz.saskonline.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.mrz.saskonline.R
 import com.mrz.saskonline.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Инициализация биндинга для активити
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Инициализация тулбара
+        setSupportActionBar(binding.toolbar)
+
+        // Инициализация NavHostFragment
+        navController = findNavController(R.id.fragmentContainerView)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
 }
